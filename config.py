24db6b99b4d429d92e23e1b707f0370d058f2bea
mohamedrafat9 <raftmhmaa@gmail.com>
2026-09-09 07:42:47 +0300

    add some

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     openrouter_model: str
     openrouter_api_key: str
+    cerebras_model: str
+    cerebras_api_key: str
+    cerebras_url: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
