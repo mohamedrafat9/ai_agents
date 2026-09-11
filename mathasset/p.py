770b652c7cbefd6fb67907c2e6a64acccfa916f7
mohamedrafat9 <raftmhmaa@gmail.com>
2026-09-11 10:15:29 +0300

    SOC

--- mathasset/p.py
+++ mathasset/p.py
@@ -1,40 +1,23 @@
 from langchain.agents import create_agent
 from langchain.tools import tool
+from mathasset.tools import divide_numbers, multiply_numbers, subtract_numbers, add_numbers
 from src.llm.factory import get_llm
 
-
-llm = get_llm()
-
-
-@tool
-def add_numbers(
-    numbers: list[float],
-    use_absolute: bool = False,
-) -> float:
-    """Add all numbers in the provided list."""
-
-    if use_absolute:
-        numbers = [abs(number) for number in numbers]
-
-    return sum(numbers)
-
+llm = get_llm(provider="groq")
 
 agent = create_agent(
     model=llm,
-    tools=[add_numbers],
+    tools=[add_numbers, subtract_numbers, multiply_numbers, divide_numbers],
+    system_prompt="You are a helpful assistant that can perform basic arithmetic operations. You have access to the following tools: add_numbers, subtract_numbers, multiply_numbers, divide_numbers. Use these tools to answer user queries.",
 )
 
-
+user_input="divide_numbers([10, 2])"
 for state in agent.stream(
     {
         "messages": [
             {
                 "role": "user",
-                "content": (
-                    "In 2023, the US GDP was approximately $27.72 trillion, "
-                    "Canada's was around $2.14 trillion, and Mexico's was "
-                    "about $1.79 trillion. What is the total?"
-                ),
+                "content": user_input
             }
         ]
     },
