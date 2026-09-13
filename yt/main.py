cfcbe0570a389578b0a6e94ce777f5c45f00852e
mohamedrafat9 <raftmhmaa@gmail.com>
2026-09-14 02:39:44 +0300

    ytapi

---
+++ yt/main.py
@@ -0,0 +1,83 @@
+from src.llm.factory import get_llm
+from langchain.agents import create_agent
+import re
+from pytube import YouTube , Search
+from youtube_transcript_api import YouTubeTranscriptApi
+from langchain_core.tools import tool
+import yt_dlp
+from typing import List, Dict
+from langchain_core.messages import HumanMessage
+from langchain_core.messages import ToolMessage
+import json
+import warnings
+warnings.filterwarnings("ignore")
+import logging
+pytube_logger = logging.getLogger('pytube')
+pytube_logger.setLevel(logging.ERROR)
+yt_dpl_logger = logging.getLogger('yt_dlp')
+yt_dpl_logger.setLevel(logging.ERROR)
+import arabic_reshaper
+from bidi.algorithm import get_display
+
+
+llm=get_llm(provider="groq")
+tools=[]
+
+@tool
+def extract_video_id(url: str) -> str:
+    """
+    Extracts the 11-character YouTube video ID from a URL.
+    Args:
+        url (str): A YouTube URL containing a video ID.
+    Returns:
+        str: Extracted video ID or error message if parsing fails.
+    """
+    # Regex pattern to match video IDs
+    pattern = r'(?:v=|be/|embed/)([a-zA-Z0-9_-]{11})'
+    match = re.search(pattern, url)
+    return match.group(1) if match else "Error: Invalid YouTube URL"
+
+# print(extract_video_id.invoke({
+#     "url": "https://www.youtube.com/watch?v=ZWdF4gHC214"
+# }))
+tools.append(extract_video_id)
+
+@tool
+def fetch_transcript(video_id: str, language: str = "en") -> str:
+    """
+    Fetches the transcript of a YouTube video.
+    Args:
+        video_id (str): The YouTube video ID (e.g., "dQw4w9WgXcQ").
+        language (str): Language code for the transcript (e.g., "en", "es").
+    Returns:
+        str: The transcript text or an error message.
+    """
+    
+    try:
+        ytt_api = YouTubeTranscriptApi()
+        transcript = ytt_api.fetch(video_id, languages=[language])
+        return " ".join([snippet.text for snippet in transcript.snippets])
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+tools.append(fetch_transcript)
+
+
+def fix_arabic(text: str) -> str:
+    reshaped_text = arabic_reshaper.reshape(text)
+    return get_display(reshaped_text)
+
+# res=fetch_transcript.invoke({
+#     "video_id": "ZWdF4gHC214",
+#     "language": "ar"
+# })
+# print(fix_arabic(res))
+
+
+agent =create_agent(
+        model=llm,
+        tools=tools,
+        system_prompt="You are a helpful YouTube assistant.",
+    )
+
+        
